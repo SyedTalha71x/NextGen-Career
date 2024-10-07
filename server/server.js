7970df97463df7dfa0d4fb6223062467f511f644
SyedTalha71x <syedtalha71x@gmail.com>
2024-10-07 08:29:30 +0000

    fix production bugs

--- server/server.js
+++ server/server.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(cors());
 
 // Routes Setup
-app.use('/api', userRoutes)
+app.use('/', userRoutes)
 
 // server setup in action
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, '0.0.0.0', ()=>{
     console.log(`Server is listening on http://localhost:${PORT}`)
 })
